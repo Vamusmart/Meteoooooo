6e6525360dab8c5820d1d709e7313ddb4eac7d8e
Administrator <administrator@CN-4.lan>
2019-03-19 14:21:02 +0000

    Updates

---
+++ precache-manifest.c3388e115b0a65cccea15ad6379300a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d63da2850aff60d5a60b",
+    "url": "/Meteo/static/js/runtime~main.c7322d55.js"
+  },
+  {
+    "revision": "75102fda9d48d89f35cb",
+    "url": "/Meteo/static/js/main.34132e12.chunk.js"
+  },
+  {
+    "revision": "3eba26b8ca2331f0f15f",
+    "url": "/Meteo/static/js/2.345dc1e5.chunk.js"
+  },
+  {
+    "revision": "75102fda9d48d89f35cb",
+    "url": "/Meteo/static/css/main.553849aa.chunk.css"
+  },
+  {
+    "revision": "7131bfa40dd85d6fa2a2dfd3a9787780",
+    "url": "/Meteo/index.html"
+  }
+];

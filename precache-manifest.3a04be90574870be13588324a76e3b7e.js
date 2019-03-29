6edb825c401c9c6f15bbf36f9176763a28cca012
Administrator <administrator@CN-4.lan>
2019-03-29 13:33:26 +0000

    Updates

---
+++ precache-manifest.3a04be90574870be13588324a76e3b7e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/Meteo/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "885fda85f0771425a149",
+    "url": "/Meteo/static/css/main.81ce6195.chunk.css"
+  },
+  {
+    "revision": "d63da2850aff60d5a60b",
+    "url": "/Meteo/static/js/runtime~main.c7322d55.js"
+  },
+  {
+    "revision": "43764a29a3c50cb2f9a0d544391084bd",
+    "url": "/Meteo/static/media/bg1.43764a29.jpg"
+  },
+  {
+    "revision": "0772b68d74ffb64e9215",
+    "url": "/Meteo/static/js/2.ecaaa552.chunk.js"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/Meteo/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "885fda85f0771425a149",
+    "url": "/Meteo/static/js/main.b8540ea5.chunk.js"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/Meteo/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/Meteo/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/Meteo/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "0772b68d74ffb64e9215",
+    "url": "/Meteo/static/css/2.da8aad0d.chunk.css"
+  },
+  {
+    "revision": "7d2a041968b371305b19e199b982b377",
+    "url": "/Meteo/index.html"
+  }
+];
